0e3ba0d8ab1bb1edda5d8cd54e79e9cff449841a
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2024-05-03 21:36:39 -0300

    feat: add inputs to register page

--- src/Pages/Cadastro/index.js
+++ src/Pages/Cadastro/index.js
@@ -1,7 +1,28 @@
-const PageCadastro = () => (
-    <div>
-        Página de Cadastro
-    </div>
-)
+import { useState } from "react"
+
+const PageCadastro = () => {
+    const [userData, setUserData] = useState({
+        name: "",
+        lastName: "",
+        birthDate: "",
+        email: "",
+        pwd: ""
+    })
+
+    const handleOnChange = (e) => {
+        e.preventDefault()
+        setUserData(prevState => ({...prevState, [e.target.name]: e.target.value}))
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="Nome" name="name" value={userData.name} onChange={handleOnChange}/>
+            <input type="text" placeholder="Sobrenome" name="lastName" value={userData.lastName} onChange={handleOnChange}/>
+            <input type="date" placeholder="Data de Nascimento" name="birthDate" value={userData.birthDate} onChange={handleOnChange}/>
+            <input type="email" placeholder="E-mail" name="email" value={userData.email} onChange={handleOnChange}/>
+            <input type="password" placeholder="Senha" name="pwd" value={userData.pwd} onChange={handleOnChange}/>
+        </div>
+    )
+}
 
 export default PageCadastro
